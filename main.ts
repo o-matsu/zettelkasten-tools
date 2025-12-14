6aebaece9c04591a9ba0c8e4e4a6d62507717bf9
o-matsu <matsushita.ryosuke@pine-app.studio>
2025-12-14 16:13:22 +0900

    Refactor MyPlugin to ZettelkastenTools, update settings interface, and streamline ribbon icon functionality for creating Zettelkasten and literature notes. Remove unused modal and command code for improved clarity.

--- main.ts
+++ main.ts
@@ -2,7 +2,6 @@ import {
 	App,
 	Editor,
 	MarkdownView,
-	Modal,
 	Notice,
 	Plugin,
 	PluginSettingTab,
@@ -14,21 +13,40 @@ import setAliases from "libs/setAliases";
 import createNewZettelkasten from "libs/createNewZettelkasten";
 import createNewLiterature from "libs/createNewLiterature";
 
-// Remember to rename these classes and interfaces!
-interface MyPluginSettings {
+interface Settings {
 	literatureFolder: string;
 	zettelkastenFolder: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
+const DEFAULT_SETTINGS: Settings = {
 	literatureFolder: "00_Insights",
 	zettelkastenFolder: "10_Zettels",
 };
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class ZettelkastenTools extends Plugin {
+	settings: Settings;
 
 	async onload() {
+		await this.loadSettings();
+
+		const zettelkastenIconEl = this.addRibbonIcon(
+			"plus",
+			"New Zettelkasten",
+			async (_evt: MouseEvent) => {
+				await createNewZettelkasten(this.app, this.settings.zettelkastenFolder);
+				new Notice("new zettelkasten note created", 2000);
+			}
+		);
+		zettelkastenIconEl.addClass("accent-ribbon-class");
+		this.addRibbonIcon(
+			"file-plus-2",
+			"New Literature",
+			async (_evt: MouseEvent) => {
+				await createNewLiterature(this.app, this.settings.literatureFolder);
+				new Notice("new literature note created", 2000);
+			}
+		);
+
 		this.addCommand({
 			id: "obsidian-tools-on-save",
 			name: "On save",
@@ -79,87 +97,23 @@ export default class MyPlugin extends Plugin {
 			},
 		});
 
-		// ---
-
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const zettelkastenIconEl = this.addRibbonIcon(
-			"plus",
-			"New Zettelkasten",
-			async (_evt: MouseEvent) => {
-				// Called when the user clicks the icon.
-				await createNewZettelkasten(this.app, this.settings.zettelkastenFolder);
-				new Notice("new zettelkasten note created", 2000);
-			}
-		);
-		// Perform additional things with the ribbon
-		zettelkastenIconEl.addClass("accent-ribbon-class");
-		this.addRibbonIcon(
-			"file-plus-2",
-			"New Literature",
-			async (_evt: MouseEvent) => {
-				// Called when the user clicks the icon.
-				await createNewLiterature(this.app, this.settings.literatureFolder);
-				new Notice("new literature note created", 2000);
-			}
-		);
-
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText("Status Bar Text");
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: "open-sample-modal-simple",
-			name: "Open sample modal (simple)",
-			callback: () => {
-				new SampleModal(this.app).open();
-			},
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: "sample-editor-command",
-			name: "Sample editor command",
-			editorCallback: (editor: Editor, _view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection("Sample Editor Command");
-			},
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: "open-sample-modal-complex",
-			name: "Open sample modal (complex)",
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView =
-					this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			},
-		});
+		// const statusBarItemEl = this.addStatusBarItem();
+		// statusBarItemEl.setText("Status Bar Text");
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new SettingTab(this.app, this));
 
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, "click", (evt: MouseEvent) => {
-			console.log("click", evt);
-		});
+		// this.registerDomEvent(document, "click", (evt: MouseEvent) => {
+		// 	console.log("click", evt);
+		// });
 
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(
-			window.setInterval(() => console.log("setInterval"), 5 * 60 * 1000)
-		);
+		// this.registerInterval(
+		// 	window.setInterval(() => console.log("setInterval"), 5 * 60 * 1000)
+		// );
 	}
 
 	onunload() {}
@@ -173,26 +127,26 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+// class SampleModal extends Modal {
+// 	constructor(app: App) {
+// 		super(app);
+// 	}
 
-	onOpen() {
-		const { contentEl } = this;
-		contentEl.setText("Woah!");
-	}
+// 	onOpen() {
+// 		const { contentEl } = this;
+// 		contentEl.setText("Woah!");
+// 	}
 
-	onClose() {
-		const { contentEl } = this;
-		contentEl.empty();
-	}
-}
+// 	onClose() {
+// 		const { contentEl } = this;
+// 		contentEl.empty();
+// 	}
+// }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class SettingTab extends PluginSettingTab {
+	plugin: ZettelkastenTools;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: ZettelkastenTools) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
